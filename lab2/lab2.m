9b59db981c2e8f443aeacee6cba867cf7545bf30
allancao <acao737@gmail.com>
2015-03-08 22:49:58 -0400

    Update lab2.m

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -1,3 +1,6 @@
+clear all
+load('lab2_1.mat');
+
 %Parametric Estimation Gaussian - Set A
 tMu = 5;
 tSigma = 1;
@@ -86,3 +89,57 @@ hold on
 Plots.plotExpo(tLambda, 'r');
 title('Estimated Unif Dist vs. Actual Expo Dist Set B');
 legend('Est Unif', 'Act Expo');
+
+%Non-parametric Estimation 
+
+tMu = 5;
+tSigma = 1;
+tLambda = 1;
+
+stdDev1 = 0.1;
+stdDev2 = 0.4;
+x = 0:.1:9.9;
+N = length(a);
+
+parzen1a = zeros(1,100);
+parzen2a = zeros(1,100);
+parzen1b = zeros(1,100);
+parzen2b = zeros(1,100);
+
+
+for j=1:100
+    parzenSum1a = 0;
+    parzenSum2a = 0;
+    parzenSum1b = 0;
+    parzenSum2b = 0;
+
+    for i = 1:N
+        parzenSum1a = parzenSum1a + ((1/(stdDev1 * sqrt(2*pi))) * exp(-.5 * (((j/10 - a(i))/(stdDev1))^2)));
+        parzenSum2a = parzenSum2a + ((1/(stdDev2 * sqrt(2*pi))) * exp(-.5 * (((j/10 - a(i))/(stdDev2))^2)));
+        parzenSum1b = parzenSum1b + ((1/(stdDev1 * sqrt(2*pi))) * exp(-.5 * (((j/10 - b(i))/(stdDev1))^2)));
+        parzenSum2b = parzenSum2b + ((1/(stdDev2 * sqrt(2*pi))) * exp(-.5 * (((j/10 - b(i))/(stdDev2))^2)));
+    end
+    parzen1a(j) = (1/N) * parzenSum1a;
+    parzen2a(j) = (1/N) * parzenSum2a;
+    parzen1b(j) = (1/N) * parzenSum1b;
+    parzen2b(j) = (1/N) * parzenSum2b;
+
+end
+
+figure 
+plot(x, parzen1a, 'b');
+hold on
+plot(x, parzen2a, 'r');
+hold on
+Plots.plotGauss(tMu, tLambda, 'g');
+title('Parzen gaussian window estimation Set A');
+legend('StdDev = 0.1', 'StdDev = 0.4', 'Act Dist');
+
+figure
+plot(x, parzen1b, 'b');
+hold on
+plot(x, parzen2b, 'r');
+hold on
+Plots.plotExpo(tLambda, 'g');
+title('Parzen gaussian window estimation Set B');
+legend('StdDev = 0.1', 'StdDev = 0.4', 'Act Dist');e
