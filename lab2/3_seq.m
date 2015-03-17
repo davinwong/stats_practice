eee8c4966b1932bd2c98abf28fe66383789384cc
davinwong <davintw@gmail.com>
2015-03-16 20:12:02 -0400

    sequential classifier works

--- lab2/3_seq.m
+++ lab2/3_seq.m
@@ -1,5 +1,7 @@
 load lab2_3.mat
 
+% sequential classifier
+
 j = 1;
 
 % proto_a, proto_b, n_aB, n_bA
@@ -7,6 +9,7 @@ g = [];
 
 while length(a) > 0 | length(b) > 0
 
+	% find prototypes for a, b
 	if length(a) > 0
 		r_a = randi([1, length(a)]);
 		proto_a = a(r_a,:);
@@ -23,7 +26,6 @@ while length(a) > 0 | length(b) > 0
 
 	% error: number in a incorrectly classified as B
 	n_aB = 0;
-
 	n_bA = 0;
 
 	% next version (not yet correct with current discriminants)
@@ -32,32 +34,36 @@ while length(a) > 0 | length(b) > 0
 
 	% iterate a
 	for i=1:length(a)
-		d_a = (a(i,:) - proto_a)' * (a(i,:) - proto_a)
-		d_b = (a(i,:) - proto_b)' * (a(i,:) - proto_b)
+		d_a = (a(i,:) - proto_a) * (a(i,:) - proto_a)';
+		d_b = (a(i,:) - proto_b) * (a(i,:) - proto_b)';
 
+		% MED: assign class of lower distance
 		if d_a < d_b
+			% correctly classified. will not be added to next version
 			;
 		else
 			% error
 			n_aB = n_aB + 1;
 
 			% add to next version
-			next_a(end+1,:) = a(i,:)
+			next_a(end+1,:) = a(i,:);
 		end
 	end
 
 	% iterate b
 	for i=1:length(b)
-		d_a = (b(i,:) - proto_a)' * (b(i,:) - proto_a)
-		d_b = (b(i,:) - proto_b)' * (b(i,:) - proto_b)
+		d_a = (b(i,:) - proto_a) * (b(i,:) - proto_a)';
+		d_b = (b(i,:) - proto_b) * (b(i,:) - proto_b)';
 
+		% MED: assign class of lower distance
 		if d_a < d_b
 			% error
 			n_bA = n_bA + 1;
 
 			% add to next version
-			next_b(end+1,:) = b(i,:)
+			next_b(end+1,:) = b(i,:);
 		else
+			% correctly classified. will not be added to next version
 			;
 		end
 	end
@@ -71,20 +77,49 @@ while length(a) > 0 | length(b) > 0
 	g(j,:) = [proto_a, proto_b, n_aB, n_bA];
 	j = j + 1;
 
-	% use next version. removes a, b elements that were already correctly classified
-	a = next_a;
-	b = next_b;
+	% use one of the next version.
+	% remove correctly classified elements on the side of discriminant that has no errors (n = 0)
+	if n_aB == 0
+		% a = a;
+		b = next_b;
+	end
+
+	if n_bA == 0
+		a = next_a;
+		% b = b;
+	end
 
 end
 
 
-disp('GGG');
+disp('g');
 disp(g);
 
-disp('LENGTHS');
-disp(length(a));
-disp(length(b));
-% disp('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-% disp(a);
-% disp('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB');
-% disp(b);
+
+
+
+
+% disp('jjjj');
+% disp(j);
+
+% disp('length a');
+% disp(length(a));
+% disp('length b');
+% disp(length(b));
+
+
+% disp('b');
+% disp(b(i,:));
+% disp('proto_a');
+% disp(proto_a);
+% disp('proto_b');
+% disp(proto_b);
+% disp('(b(i,:) - proto_b)"');
+% disp((b(i,:) - proto_b)');
+% disp('(b(i,:) - proto_b)');
+% disp((b(i,:) - proto_b));
+
+% disp('d_a');
+% disp(d_a);
+% disp('d_b');
+% disp(d_b);
