7e6c2580b1dbfe9ea3e0ec8bfeb075056c673ca2
davinwong <davintw@gmail.com>
2015-03-17 02:12:31 -0400

    part 3 computes classifiers limit 1-5, 20 times each

--- lab2/4_3_main.m
+++ lab2/4_3_main.m
@@ -1,6 +1,20 @@
 load lab2_3.mat
 
-g = get_sequential_discriminants(a, b);
+data = vertcat(a, b);
 
 %%% Part 4-3 %%%
+
+for limit_discrim=1:5
+	disp('limit');
+	disp(limit_discrim)
+
+	for i=1:20
+		disp(i);
+		g = get_sequential_discriminants(a, b, limit_discrim);
+		y = sequential_classify(g, data);
+
+	end
+end
+
+
 
