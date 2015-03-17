fd2deb87a99051c56f2eb865ca084f31ac55398b
davinwong <davintw@gmail.com>
2015-03-17 01:40:37 -0400

    main cleanup, whitespae

--- lab2/4_main.m
+++ lab2/4_main.m
@@ -3,25 +3,8 @@ load lab2_3.mat
 
 g = get_sequential_discriminants(a, b);
 
-disp('g');
-disp(g);
 
-% data = vertcat(a, b);
-
-% y = sequential_classify(g, data);
-
-% disp(y);
-
-
-
-
-% Part 4-1
-
-
-% x = vertcat(a(:, 1), b(:, 1));
-% y = vertcat(a(:, 2), b(:, 2));
-% c = vertcat( zeros(length(a)) + 10, zeros(length(b)) + 70);
-
+%%% Part 4-1 %%%
 
 scatter(a(:, 1), a(:, 2), 'r');
 hold on
@@ -29,7 +12,6 @@ scatter(b(:, 1), b(:, 2), 'g');
 hold on
 
 for j=1:length(g)
-
 	ax = g(j, 1);
 	ay = g(j, 2);
 	bx = g(j, 3);
@@ -37,20 +19,23 @@ for j=1:length(g)
 
 	x = linspace(0,600,10);
 	y = (2*ax*x - ax^2 - ay^2 - 2*bx*x + bx^2 + by^2) / (-2*ay + 2*by);
-	disp(y);
+
 	plot(x,y,'b');
 	hold on
-
 end
 
 axis([0 600 0 600]);
-
 print('4_1_3.png');
-
 save('4_1_3.mat', 'g')
-
 
 pause;
 
 
+%%% Part 4-2 %%%
+
+% data = vertcat(a, b);
+% y = sequential_classify(g, data);
+
+
+%%% Part 4-3 %%%
 
