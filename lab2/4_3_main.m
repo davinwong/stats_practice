4fbab20afec81514cf05005ae7bb0ab3082813f7
davinwong <davintw@gmail.com>
2015-03-17 02:25:35 -0400

    trying to calculate error rate

--- lab2/4_3_main.m
+++ lab2/4_3_main.m
@@ -4,17 +4,53 @@ data = vertcat(a, b);
 
 %%% Part 4-3 %%%
 
-for limit_discrim=1:5
-	disp('limit');
-	disp(limit_discrim)
+error_rates = [];
 
+for limit_discrim=1:5
+	% disp('limit');
+	% disp(limit_discrim)
+
+	% run multiple tests for each limit
 	for i=1:20
-		disp(i);
+		% disp(i);
 		g = get_sequential_discriminants(a, b, limit_discrim);
 		y = sequential_classify(g, data);
 
+		% calculate error rate
+		errors = 0;
+
+		% a
+		for d=1:length(a)
+			if y(d,1) == 0
+				;
+			else
+				errors = errors + 1;
+			end
+		end
+
+		% b
+		for d=(length(a)+1):length(b)
+			if y(d,1) == 1
+				;
+			else
+				errors = errors + 1;
+			end
+		end
+
+		disp('err');
+		disp(errors);
+		disp('lengths');
+		disp(length(a));
+		disp(length(b));
+		disp('error_rate');
+		disp(errors / (length(a) + length(b)))
+
+		error_rates(limit_discrim, i) = errors / (length(a) + length(b));
+
 	end
 end
+
+disp(error_rates);
 
 
 
