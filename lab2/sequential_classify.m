72c3862b07c70334812c71ea5325cddfd649123e
davinwong <davintw@gmail.com>
2015-03-17 02:10:46 -0400

    update classification to handle not having all discriminants

--- lab2/sequential_classify.m
+++ lab2/sequential_classify.m
@@ -8,7 +8,7 @@ function y_out = sequential_classify(g, data)
 	i_data = 1;
 	j_discrim = 1;
 
-	while i_data <= length(data)
+	while i_data <= size(data,1)
 
 		proto_a = g(j_discrim, 1:2);
 		proto_b = g(j_discrim, 3:4);
@@ -22,11 +22,25 @@ function y_out = sequential_classify(g, data)
 			i_data = i_data + 1;
 			j_discrim = 1;
 
-		else if d_a < d_b & g(j_discrim, 6) == 0
+		elseif d_a < d_b & g(j_discrim, 6) == 0
 			% classify as A. start next data point.
 			y(i_data, 1) = 0;
 			i_data = i_data + 1;
 			j_discrim = 1;
+
+		elseif j_discrim == size(g,1)
+			% last discriminant reached
+			if g(j_discrim, 5) == 0
+				% classify as A. start next data point.
+				y(i_data, 1) = 0;
+				i_data = i_data + 1;
+				j_discrim = 1;
+			else
+				% classify as B. start next data point.
+				y(i_data, 1) = 1;
+				i_data = i_data + 1;
+				j_discrim = 1;
+			end
 
 		else
 			% try next discriminant function
